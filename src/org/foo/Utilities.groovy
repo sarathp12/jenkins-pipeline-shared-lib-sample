25d809457f5b3b251975db4cc7fa687d6c88fbd5
parthasarathy pavuluri <parthap@parthasarathys-MacBook-Pro.local>
2020-11-08 16:11:29 -0500

    test-library

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -3,7 +3,7 @@ class Utilities implements Serializable {
   def steps
   Utilities(steps) {this.steps = steps}
   def checkOutFrom(repo) {
-    git url: "git@github.com:sarathp12/${repo}"
+    git url: "https://github.com/sarathp12/${repo}.git"
   }
 
 }
