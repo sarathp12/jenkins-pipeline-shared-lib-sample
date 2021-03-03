e11447c87464918e0bc17aff10dce7778ac15586
devOps-brain <devOps-brain@psp.org>
2021-03-03 15:59:56 -0500

    updated print strings

--- src/org/foo/Ansi.groovy
+++ src/org/foo/Ansi.groovy
@@ -71,8 +71,12 @@ class Ansi {
     static final String	LIGHT_PURPLE    = "\u001B[1;35m"
     static final String	LIGHT_CYAN      = "\u001B[1;36m"
 
-    static String color(String text, String ansiValue) {
-        ansiValue + text + NORMAL
+    def colors = [black: 30, red: 31, green: 32, yellow: 33, blue: 34, magenta: 35, cyan: 36, white: 37]
+
+    static String color(String text, String color) {
+        //return(ansiValue + text + NORMAL)
+        return new String((char) 27) + "[${colors[color]}m${text}" + new String((char) 27) + "[0m"
+
     }
 
 }
